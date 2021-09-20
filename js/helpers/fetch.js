d1f59d7940c9fef36b8945eaae9f0098be8f59ea
Johan Paul Barahona Santos <72539686+panlar@users.noreply.github.com>
2021-09-20 17:17:32 -0600

    Remplazando cualquier "http" por "https"

--- js/helpers/fetch.js
+++ js/helpers/fetch.js
@@ -1,16 +1,7 @@
 import { ls } from "./consts.js";
 
 export default async function getData(url) {
-  if (ls.getItem("fetch") === null) {
-    ls.setItem("fetch", "0");
-  }
-
-  let res = await fetch(url),
+  let res = await fetch(url.replace(/^https|http/gi, "https")),
     json = await res.json();
-
-  if (!(url.includes("jsdelivr"))) {
-    ls.setItem("fetch", JSON.parse(ls.getItem("fetch")) + 1);
-  }
-
   return json;
 }
